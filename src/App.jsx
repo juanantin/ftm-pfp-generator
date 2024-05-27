5e557f6452afca19bd7a29456e0137df9f1f1811
NaNa2005 <hovvathana1@gmail.com>
2024-05-27 15:48:36 +0700

    change donwload name

--- src/App.jsx
+++ src/App.jsx
@@ -264,7 +264,7 @@ function App() {
   const saveImageToLocal = () => {
     if (canvas) {
       const link = document.createElement("a");
-      link.download = "Pepewifhat.png";
+      link.download = "cok_meme.png";
       link.href = saveImageToDataURL();
       document.body.appendChild(link);
       link.click();
